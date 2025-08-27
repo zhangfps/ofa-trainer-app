5a4120fde71aeb71ab635e8d113f2fa07cd6a7bc
zhangfps <noe.dominic.gg@gmail.com>
2025-08-27 09:50:47 +0800

    React + NEXT + Chakra Starting Point 🚀

--- app/+not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function NotFoundScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>404 - Page not found</Text>
-    </View>
-  );
-}
--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function Home() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
